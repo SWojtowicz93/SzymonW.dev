101f9e04740bc8990f08b26f3f095c67dc32aa24
szymon.wojtowicz93 <szymon.wojtowicz93@gmail.com>
2023-01-22 23:51:27 +0100

    add main section and about me

---
+++ js/slick.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.header__slide-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+
+    });
+})
+$(document).ready(function () {
+    $('.header__slide-img-two').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+
+    });
+})
